a01d9726d5412be8b27bfb0c4179fccfb3ad9b8e
shahid-anwar <shahidanwaransari007@gnmail.com>
2026-05-14 22:57:53 +0530

    Fixed production CORS

--- server/src/server.js
+++ server/src/server.js
@@ -20,7 +20,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://ticket-managament-lite.vercel.app/",
+      "https://ticket-managament-lite.vercel.app",
     ],
 
     credentials: true,
